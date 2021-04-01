72ccb379b3770ead35d7c7f61c6ab80f810ac150
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-03-31 19:22:46 -0700

    add image, Card.Link

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -8,17 +8,19 @@ const Works = () => {
         <Container>
         <CardGroup className="card-group">
             <Card style={{ width: '18rem' }} className="card">
-                <Card.Img src="./works-img/cricket.png" variant="top" alt="photo" />
+                <Card.Img src="public/images/memoApp.png" variant="top" alt="photo" />
                 <Card.Body>
                     <Card.Title>React</Card.Title>
                     <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the card's content.
                     </Card.Text>
+                    <Card.Link href="https://github.com/sachiko0811/MemoApp">
                     <Button 
                     variant="primary"
                     // onClick={goWebsite}
                     >WEBSITE</Button>
+                    </Card.Link>
                 </Card.Body>
             </Card>
 
